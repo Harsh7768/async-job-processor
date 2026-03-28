7655d00f2c5bf179932d8f8fc835db4fa8097620
Harsh <harshdtu2002@gmail.com>
2026-03-28 18:49:23 +0530

    Prepare project for deployment

--- app/db.py
+++ app/db.py
@@ -1,11 +1,6 @@
 import psycopg
+import os
 
 
 def get_connection():
-    return psycopg.connect(
-        dbname="job_db",
-        user="postgres",
-        password="1234",
-        host="localhost",
-        port="5432"
-    )
+    return psycopg.connect(os.getenv("DATABASE_URL"))
